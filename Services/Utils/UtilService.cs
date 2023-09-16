acf735c2f4312e47bda6c52afdce55c0133076dc
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-16 05:36:24 -0600

    chore: :sparkles: utils service encrypt and generate token

---
+++ Services/Utils/UtilService.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace job_opportunities_asp_react.Services.Utils
+{
+    public static class UtilService
+    {
+        public static string ConvertSHA256(string text)
+        {
+            string hash = string.Empty;
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                //obtener el hash recibido
+                byte[] hashValue = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+                // convertir array de bytes en cadena de texto
+                foreach (byte b in hashValue)
+                {
+                    hash += $"{b:X2}";
+                }
+            }
+            return hash;
+        }
+
+        public static string GenerateToken() => Guid.NewGuid().ToString("N");
+
+    }
+}
