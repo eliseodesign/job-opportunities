55a2f126b97d6ca22227afd39b1c8bb8b4771d09
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-16 05:36:59 -0600

    feat: :sparkles: create applicant service

---
+++ Services/ApplicantService.cs
@@ -0,0 +1,104 @@
+﻿using job_opportunities_asp_react.Models.Entities;
+using job_opportunities_asp_react.Models.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace job_opportunities_asp_react.Services
+{
+    public class ApplicantService : IApplicantService
+    {
+        private readonly IGenericRepository<Applicant> repo;
+        public ApplicantService(IGenericRepository<Applicant> _repo)
+        {
+            repo = _repo;
+        }
+
+        public Task<bool> Create(Applicant model) => repo.Create(model);
+        
+        public Task<bool> Delete(int id) => repo.Delete(id);
+
+        public Task<IQueryable<Applicant>> GetAll() => repo.GetAll();
+
+        public Task<Applicant> GetById(int id) => repo.GetById(id);
+
+        public Task<bool> Update(Applicant model) => repo.Update(model);
+
+        /// <sumary>
+        /// Login de usuario
+        /// </sumary>
+        public async Task<Applicant> Validate(string email, string password)
+        {
+            try
+            {
+                var applicants = await repo.GetAll();
+                var appl =  await applicants.FirstAsync(a => a.Email == email && a.Password == password);
+                return appl;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+
+        /// <sumary>
+        /// Login de usuario
+        /// </sumary>
+        public async Task<Applicant> GetByEmail(string email)
+        {
+            try
+            {
+                var applicants = await repo.GetAll();
+                var appl = await applicants.FirstAsync(a => a.Email == email);
+                return appl;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+
+        /// <sumary>
+        /// Restablecer contraseña
+        /// </sumary>
+        public async Task<bool> RestartAccount(bool restart, string password, string token)
+        {
+            try
+            {
+                var aplicants = await repo.GetAll();
+                Applicant existingApplicant = await aplicants.FirstAsync(a => a.Token == token);
+
+                existingApplicant.RestartAccount = restart;
+                existingApplicant.Password = password;
+
+                await repo.Update(existingApplicant);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        /// <sumary>
+        /// Restablecer contraseña
+        /// </sumary>
+        public async Task<bool> ConfirmarToken(string token)
+        {
+            try
+            {
+                var aplicants = await repo.GetAll();
+                Applicant existingApplicant = await aplicants.FirstAsync(a => a.Token == token);
+
+                existingApplicant.ConfirmAccount = true;
+
+                await repo.Update(existingApplicant);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Services/IApplicantService.cs
@@ -0,0 +1,17 @@
+﻿using job_opportunities_asp_react.Models.Entities;
+
+namespace job_opportunities_asp_react.Services
+{
+    public interface IApplicantService
+    {
+        Task<bool> Create(Applicant model);
+        Task<bool> Update(Applicant model);
+        Task<bool> Delete(int id);
+        Task<IQueryable<Applicant>> GetAll();
+        Task<Applicant> GetById(int id);
+        Task<Applicant> Validate(string email, string password);
+        Task<Applicant> GetByEmail(string email);
+        Task<bool> RestartAccount(bool restart, string password, string token);
+        Task<bool> ConfirmarToken(string token);
+    }
+}
