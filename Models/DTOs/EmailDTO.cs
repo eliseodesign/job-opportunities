70abbe9b6ba90e81c961864952e9810c2fdfdf50
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-17 18:44:28 -0600

    chore: :sparkles: add EmailDTO

---
+++ Models/DTOs/EmailDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace job_opportunities_asp_react.Models.DTOs
+{
+    public class EmailDTO
+    {
+        public required string To { get; set; }
+        public required string Subject { get; set; }
+        public required string Content { get; set; }
+    }
+}
