62573c76117c061a93a8a9bd248511be3c85fc02
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-19 17:35:38 -0600

    chore: :sparkles: add service interface

---
+++ Services/Utils/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using  job_opportunities_asp_react.Models.DTOs;
+
+namespace job_opportunities_asp_react.Services.Utils;
+
+public interface IEmailService
+{
+  bool SendEmail(EmailDTO emailDTO);
+}
