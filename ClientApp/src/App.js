7750c77aa3d5547672b4b4ff102143b65a374d3f
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-20 21:02:40 -0600

    chore: :sparkles: install and config tailwind

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Layout } from './components/Layout';
-import './custom.css';
 
 function App() {
   return (
-    <Layout>
-      <h1>Eliseo</h1>
-    </Layout>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
   )
 }
 
--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import './index.css'
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+
 
 root.render(
   <BrowserRouter basename={baseUrl}>
