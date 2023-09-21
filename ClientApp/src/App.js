bb3c566b88a1ea6205461afc67e1bff7cbd7145e
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-20 20:28:16 -0600

    fix: :hammer: clean ClientApp, dependencies and scaffold

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,22 +1,13 @@
-import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import AppRoutes from './AppRoutes';
+import React from 'react';
 import { Layout } from './components/Layout';
 import './custom.css';
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render() {
-    return (
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
-        </Routes>
-      </Layout>
-    );
-  }
+function App() {
+  return (
+    <Layout>
+      <h1>Eliseo</h1>
+    </Layout>
+  )
 }
+
+export default App
