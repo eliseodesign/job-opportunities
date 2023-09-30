14c99fc3c5e157afd3e3a89e3a872c518d425bc1
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-30 15:21:15 -0600

    chore: :sparkles: read controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using job_opportunities_asp_react.Models.Entities;
 using job_opportunities_asp_react.Models.DTOs;
 using job_opportunities_asp_react.Services;
+using job_opportunities_asp_react.Services.Interfaces;
 using job_opportunities_asp_react.Services.Utils;
 
 namespace job_opportunities_asp_react.Controllers
---
+++ Controllers/ReadContoller.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using job_opportunities_asp_react.Models.DTOs;
+using job_opportunities_asp_react.Services.Interfaces;
+using job_opportunities_asp_react.Services.Utils;
+
+namespace job_opportunities_asp_react.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReadController : ControllerBase
+    {
+        private readonly IReadOnlyService ReadService;
+
+        public ReadController(IReadOnlyService _ReadService)
+        {
+            ReadService = _ReadService;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetInfo()
+        {
+            ReadInfoDTO result = await ReadService.getAllReadInfo();
+            return Ok(Res.Provider(new {
+                Countries = result.Countries.ToList(),
+                EducationDegrees = result.EducationDegrees.ToList(),
+                EducationLevels = result.EducationLevels.ToList(),
+                EducationSubjects = result.EducationSubjects.ToList(),
+                FieldSectors = result.FieldSectors.ToList(),
+                Genders = result.Genders.ToList(),
+                MaritalStatuses = result.MaritalStatuses.ToList()
+            }, "Operación exitosa", true));
+        }
+    }
+}
