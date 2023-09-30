9775e3edc963cd5b95274b6cb5948b9f2a37af7b
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-30 13:24:23 -0600

    chore: :sparkles: add ReadOnly Repository

---
+++ Models/Repositories/IReadOnlyRepository.cs
@@ -0,0 +1,17 @@
+﻿using job_opportunities_asp_react.Models.Entities;
+namespace job_opportunities_asp_react;
+
+/// <summary>
+/// interface para repositorio de entidades solo delectura 
+/// Country, EducationDegree, EducationLevel, EducationSubject, FieldSector, Gender and Marital Status
+/// </summary>
+public interface IReadOnlyRepository
+{
+  IQueryable<Country> getCountries();
+  IQueryable<EducationDegree> getEduDegrees();
+  IQueryable<EducationLevel> getEduLevels();
+  IQueryable<EducationSubject> getEduSubjects();
+  IQueryable<FieldSector> getSectors();
+  IQueryable<Gender> getGenders();
+  IQueryable<MaritalStatus> getMaritalStatus();
+}
---
+++ Models/Repositories/ReadOnlyRepository.cs
@@ -0,0 +1,46 @@
+﻿using job_opportunities_asp_react.Models.Entities;
+
+namespace job_opportunities_asp_react;
+
+public class ReadOnlyRepository : IReadOnlyRepository
+{
+    private readonly JobOpportunitiesContext db;
+    public ReadOnlyRepository(JobOpportunitiesContext _db){
+        db = _db;
+    }
+
+    public IQueryable<Country> getCountries()
+    {
+        return db.Countries;
+    }
+
+    public IQueryable<EducationDegree> getEduDegrees()
+    {
+        return db.EducationDegrees;
+    }
+
+    public IQueryable<EducationLevel> getEduLevels()
+    {
+        return db.EducationLevels;
+    }
+
+    public IQueryable<EducationSubject> getEduSubjects()
+    {
+        return db.EducationSubjects;
+    }
+
+    public IQueryable<Gender> getGenders()
+    {
+        return db.Genders;
+    }
+
+    public IQueryable<MaritalStatus> getMaritalStatus()
+    {
+        return db.MaritalStatuses;
+    }
+
+    public IQueryable<FieldSector> getSectors()
+    {
+        return db.FieldSectors;
+    }
+}
