322e25c20f5454f13f93d62bc671962b3a8526c3
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-10-01 12:10:43 -0600

    chore: :sparkles: add register and confirm account

--- ClientApp/src/views/public/pages/auth/ConfirmAccunt.jsx
+++ ClientApp/src/views/public/pages/auth/ConfirmAccunt.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-export const ConfirmAccunt = () => {
+export const ConfirmAccount = () => {
+  const { token } = useParams();
+  const [confirmationStatus, setConfirmationStatus] = useState('Loading...');
+
+  useEffect(() => {
+    const fetchConfirmation = async () => {
+      try {
+        const response = await fetch(`/api/auth/verify?token=${token}`);
+        
+        if (!response.ok) {
+          throw new Error('No se pudo confirmar la cuenta');
+        }
+
+        const data = await response.json();
+        // Manejar la respuesta exitosa aquí si es necesario
+        setConfirmationStatus(data.message); // Cambia data.message según la estructura de la respuesta
+      } catch (error) {
+        // Manejar errores de la solicitud aquí
+        console.error(error);
+        setConfirmationStatus('Error al confirmar la cuenta');
+      }
+    };
+
+    fetchConfirmation();
+  }, [token]);
+
   return (
-    <div>ConfirmAccunt</div>
-  )
-}
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <div className="bg-white text-teal-900 p-6 rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-4 ">Confirmación de Cuenta</h1>
+        <p className="text-lg mb-4 ">Estado de confirmación: {confirmationStatus}</p>
+      </div>
+    </div>
+  );
+};
--- ClientApp/src/views/public/pages/auth/Register.jsx
+++ ClientApp/src/views/public/pages/auth/Register.jsx
@@ -1,47 +1,139 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 export function Register() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetch('api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // Enviamos los datos del formulario como JSON
+      });
+  
+      if (response.ok) {
+        const responseData = await response.json();
+  
+        if (responseData.success) {
+          // Registro exitoso, puedes redirigir al usuario a otra página o mostrar un mensaje de éxito
+          console.log('Registro exitoso:', responseData.message);
+        } else {
+          // La API devolvió un error
+          console.error('Error en el registro:', responseData.message);
+        }
+      } else {
+        // Error en la solicitud (por ejemplo, error de red o servidor no responde)
+        console.error('Error en la solicitud al servidor');
+      }
+    } catch (error) {
+      // Error en la función fetch o en el procesamiento de la respuesta
+      console.error('Error en la solicitud:', error);
+    }
+  };
+  
+
   return (
     <>
-      <section class="bg-gray-50 dark:bg-gray-900">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-
-          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+      <section className="bg-gray-50">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
+          <div className="w-full bg-white mt-4 rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                 Create an account
               </h1>
-              <form class="space-y-4 md:space-y-6" action="#">
+              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
                 <div>
-                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                  <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your e-mail</label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ${errors.email ? 'border-red-500' : ''}`}
+                    placeholder="name@company.com"
+                    {...register("email", {
+                      required: 'Email is required',
+                      pattern: {
+                        value: /^\S+@\S+$/i,
+                        message: 'Invalid email address',
+                      },
+                    })}
+                  />
+                  {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>}
                 </div>
                 <div>
-                  <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ${errors.password ? 'border-red-500' : ''}`}
+                    {...register("password", {
+                      required: 'Password is required',
+                    })}
+                  />
+                  {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>}
                 </div>
-                <div class="flex items-center justify-between">
-                  <div class="flex items-start">
-                    <div class="flex items-center h-5">
-                      <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
-                    </div>
-                    <div class="ml-3 text-sm">
-                      <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                    </div>
-                  </div>
-                  <a href="#" class="text-sm font-medium text-white text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                <div>
+                  <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-gray-900">First Name</label>
+                  <input
+                    type="text"
+                    name="firstName"
+                    id="firstName"
+                    className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ${errors.firstName ? 'border-red-500' : ''}`}
+                    placeholder="First Name"
+                    {...register("firstName", {
+                      required: 'First Name is required',
+                    })}
+                  />
+                  {errors.firstName && <p className="text-red-500 text-sm mt-1">{errors.firstName.message}</p>}
                 </div>
-                <button type="submit" class="w-full bg-blue-600 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-
-                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                <div>
+                  <label htmlFor="lastName" className="block mb-2 text-sm font-medium text-gray-900">Last Name</label>
+                  <input
+                    type="text"
+                    name="lastName"
+                    id="lastName"
+                    className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ${errors.lastName ? 'border-red-500' : ''}`}
+                    placeholder="Last Name"
+                    {...register("lastName", {
+                      required: 'Last Name is required',
+                    })}
+                  />
+                  {errors.lastName && <p className="text-red-500 text-sm mt-1">{errors.lastName.message}</p>}
+                </div>
+                <div>
+                  <label htmlFor="address1" className="block mb-2 text-sm font-medium text-gray-900">Address</label>
+                  <input
+                    type="text"
+                    name="address1"
+                    id="address1"
+                    className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ${errors.address1 ? 'border-red-500' : ''}`}
+                    placeholder="Address"
+                    {...register("address1", {
+                      required: 'Address is required',
+                    })}
+                  />
+                  {errors.address1 && <p className="text-red-500 text-sm mt-1">{errors.address1.message}</p>}
+                </div>
+                <button
+                  type="submit"
+                  className="w-full bg-blue-600 text-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Sign in
+                </button>
+                <p className="text-sm font-light text-gray-500">
+                  Don’t have an account yet? <Link to="/login" className="font-medium text-blue-600 hover:underline">Sign up</Link>
                 </p>
-
               </form>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
