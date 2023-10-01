825baaa8b3fa206249c0813f167499cd6416ad3a
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-10-01 13:04:38 -0600

    chore: :sparkles: confirm register interface

--- ClientApp/src/views/public/pages/auth/Register.jsx
+++ ClientApp/src/views/public/pages/auth/Register.jsx
@@ -1,40 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
 export function Register() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [regState, setRegState] = useState("incomplete");
 
   const onSubmit = async (data) => {
+    setRegState("sending");
     try {
       const response = await fetch('api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data), // Enviamos los datos del formulario como JSON
+        body: JSON.stringify(data),
       });
-  
+
       if (response.ok) {
         const responseData = await response.json();
-  
+
         if (responseData.success) {
-          // Registro exitoso, puedes redirigir al usuario a otra página o mostrar un mensaje de éxito
+          setRegState("complete");
           console.log('Registro exitoso:', responseData.message);
         } else {
-          // La API devolvió un error
           console.error('Error en el registro:', responseData.message);
         }
       } else {
-        // Error en la solicitud (por ejemplo, error de red o servidor no responde)
         console.error('Error en la solicitud al servidor');
       }
     } catch (error) {
-      // Error en la función fetch o en el procesamiento de la respuesta
       console.error('Error en la solicitud:', error);
     }
   };
-  
+
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Create an account
+        </h2>
+      </div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+
+        {
+          regState === "complete"
+            ?
+            (
+              <div className="text-center">
+                <p className="text-lg text-green-600">Hemos enviado un mensaje de confirmación a tu cuenta.</p>
+                <p className="mt-2">Puedes <Link to="/login" className="text-blue-600 hover:underline">iniciar sesión</Link> ahora.</p>
+              </div>
+            )
+            : <BodyRegister onSubmit={onSubmit} regState={regState} />
+        }
+
+      </div>
+    </div>
+  );
+}
+
+
+function BodyRegister({ onSubmit, regState }) {
+  const { register, handleSubmit, formState: { errors } } = useForm()
+
 
   return (
     <>
@@ -42,9 +71,7 @@ export function Register() {
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
           <div className="w-full bg-white mt-4 rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                Create an account
-              </h1>
+
               <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
                 <div>
                   <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your e-mail</label>
@@ -122,10 +149,19 @@ export function Register() {
                 </div>
                 <button
                   type="submit"
-                  className="w-full bg-blue-600 text-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                  className={`w-full bg-blue-600 text-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${regState === "sending" ? 'cursor-not-allowed opacity-75' : ''}`}
+                  disabled={regState === "sending"}
                 >
-                  Sign in
+                  {regState === "sending" ? (
+                    <div className="flex items-center justify-center">
+                      <div className="inline-block w-5 h-5 mr-3 border-t-2 border-b-2 border-r-2 border-primary-600 border-solid rounded-full animate-spin"></div>
+                      Processing...
+                    </div>
+                  ) : (
+                    'Sign in'
+                  )}
                 </button>
+
                 <p className="text-sm font-light text-gray-500">
                   Don’t have an account yet? <Link to="/login" className="font-medium text-blue-600 hover:underline">Sign up</Link>
                 </p>
